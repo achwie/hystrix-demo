3ece57d5360fcd3b4901edbdf9ea5188f3e5f40c
achwie <achwie@users.noreply.github.com>
2017-03-30 19:29:14 +0200

    Acceptance tests: improved indentation in tests

--- acceptance-tests/src/test/java/achwie/hystrixdemo/test/scenarios/CartScenarioTest.java
+++ acceptance-tests/src/test/java/achwie/hystrixdemo/test/scenarios/CartScenarioTest.java
@@ -20,22 +20,37 @@ public class CartScenarioTest extends ScenarioTest<GivenStage, WhenOnCatalogPage
 
   @Test
   public void item_that_was_added_before_login_is_still_in_cart_after_login() {
-    given().user_is_on_catalog_page().and().user_is_not_logged_in();
-    when().user_adds_item_$_to_cart_of_quantity_$(PRODUCT_ID_BOOK, 1).and().user_logs_in_as(USER_TEST).and().user_opens_cart_page();
-    then().user_should_see_cart_item_count_of(1);
+    given()
+        .user_is_on_catalog_page()
+        .and().user_is_not_logged_in();
+    when()
+        .user_adds_item_$_to_cart_of_quantity_$(PRODUCT_ID_BOOK, 1)
+        .and().user_logs_in_as(USER_TEST)
+        .and().user_opens_cart_page();
+    then()
+        .user_should_see_cart_item_count_of(1);
   }
 
   @Test
   public void cart_is_cleared_after_logout() {
-    given().user_is_logged_in_as(USER_TEST);
-    when().user_adds_item_$_to_cart_of_quantity_$(PRODUCT_ID_BOOK, 1).and().user_logs_out().and().user_opens_cart_page();
-    then().user_should_see_cart_item_count_of(0);
+    given()
+        .user_is_logged_in_as(USER_TEST);
+    when()
+        .user_adds_item_$_to_cart_of_quantity_$(PRODUCT_ID_BOOK, 1)
+        .and().user_logs_out()
+        .and().user_opens_cart_page();
+    then()
+        .user_should_see_cart_item_count_of(0);
   }
 
   @Test
   public void added_item_appears_in_cart() {
-    given().user_is_logged_in_as(USER_TEST);
-    when().user_adds_item_$_to_cart_of_quantity_$(PRODUCT_ID_BOOK, 1).and().user_opens_cart_page();
-    then().user_should_see_cart_item_count_of(1);
+    given()
+        .user_is_logged_in_as(USER_TEST);
+    when()
+        .user_adds_item_$_to_cart_of_quantity_$(PRODUCT_ID_BOOK, 1)
+        .and().user_opens_cart_page();
+    then()
+        .user_should_see_cart_item_count_of(1);
   }
 }
